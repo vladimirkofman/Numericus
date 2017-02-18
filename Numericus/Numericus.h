795f493cce2d6e5036fd7bc8ef525d5ceb266167
Vladimir Kofman <vladimir.kofman@intel.com>
2017-02-18 22:26:22 +0200

    Initial commit

---
+++ Numericus/Numericus.h
@@ -0,0 +1,19 @@
+//
+//  Numericus.h
+//  Numericus
+//
+//  Created by Vladimir Kofman on 18/02/2017.
+//  Copyright © 2017 Vladimir Kofman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Numericus.
+FOUNDATION_EXPORT double NumericusVersionNumber;
+
+//! Project version string for Numericus.
+FOUNDATION_EXPORT const unsigned char NumericusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Numericus/PublicHeader.h>
+
+
---
+++ Numericus/Numericus.swift
@@ -0,0 +1,15 @@
+//
+//  Numericus.swift
+//  Numericus
+//
+//  Created by Vladimir Kofman on 18/02/2017.
+//  Copyright © 2017 Vladimir Kofman. All rights reserved.
+//
+
+import Foundation
+
+struct Numericus {
+    func doSomething() -> Bool {
+        return true
+    }
+}
